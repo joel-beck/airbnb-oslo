156a00a9cac9f46d0e540d2d58722ad5197ae1fd
Joel Beck <joel.beck@t-online.de>
2021-11-11 16:00:07 +0100

    moved eda to separate branch

--- data-cleaning.py
+++ data-cleaning.py
@@ -1,12 +1,10 @@
-# Will clean up file, i.e. remove code cells and print statements when cleaning process is final
-
-# %%
+#%%
 import os
 
 import geopandas as gpd
 import pandas as pd
 
-# %%
+#%%
 # Overview over all Datasets
 dir = "data-raw"
 files = os.listdir(dir)
@@ -20,13 +18,13 @@ for file in files:
     print(df.iloc[:3, :5], "\n")
 
 
-# %% [markdown]
+#%% [markdown]
 # # Data Cleaning
 
-# %% [markdown]
+#%% [markdown]
 # ## Merge / Select DataFrames
 
-# %%
+#%%
 # SECTION: Data Cleaning
 # SUBSECTION: Merge / Select DataFrames
 # combine two neighbourhood dataframes
@@ -39,13 +37,13 @@ neighbourhoods_df = pd.merge(
     how="left",
 )
 
-# %%
+#%%
 # reviews.csv redundant => keep only reviews.csv.gz
 reviews_df = pd.read_csv(
     "./data-raw/reviews.csv.gz", parse_dates=["date"], index_col="listing_id"
 )
 
-# %%
+#%%
 list_1 = pd.read_csv("data-raw/listings.csv", index_col="id")
 list_2 = pd.read_csv("data-raw/listings.csv.gz", index_col="id")
 
@@ -59,25 +57,25 @@ additional_cols = list_2[list_2.columns[~list_2.columns.isin(list_1.columns)]]
 # keep all information from listings.csv and join additional information from listings.csv.gz
 listings_df = list_1.join(additional_cols)
 
-# %%
+#%%
 calendar_df = pd.read_csv(
     "data-raw/calendar.csv.gz", parse_dates=["date"], index_col="listing_id"
 )
 
-# %% [markdown]
+#%% [markdown]
 # Keep only 4 out of 7 Data Frames:
 # - listings.csv.gz
 # - calendar.csv.gz
 # - reviews.csv.gz
 # - neighbourhoods.geojson
 
-# %%
+#%%
 # all three dataframes are connected by id columns
 print(listings_df.index.nunique())
 print(calendar_df.index.nunique())
 print(reviews_df.index.nunique())
 
-# %%
+#%%
 # listings_df strict superset of unique observations
 print(calendar_df.index.isin(listings_df.index).all())
 print(reviews_df.index.isin(listings_df.index).all())
@@ -85,21 +83,21 @@ print(reviews_df.index.isin(listings_df.index).all())
 # calendar_df not a superset of reviews_df observations
 print(reviews_df.index.isin(calendar_df.index).all())
 
-# %% [markdown]
+#%% [markdown]
 # ## Convert Data Types
 
-# %%
+#%%
 # SUBSECTION: Convert Data Types
 print(reviews_df.dtypes, "\n")
 
 reviews_df = reviews_df.convert_dtypes()
 print(reviews_df.dtypes)
 
-# %%
+#%%
 neighbourhoods_df = neighbourhoods_df.convert_dtypes()
 print(neighbourhoods_df.dtypes)
 
-# %%
+#%%
 print(calendar_df.dtypes, "\n")
 
 calendar_df = calendar_df.convert_dtypes().assign(
@@ -115,7 +113,7 @@ calendar_df = calendar_df.convert_dtypes().assign(
 )
 print(calendar_df.dtypes)
 
-# %%
+#%%
 # change first_review and last_review to date and price to float
 for col in listings_df.convert_dtypes():
     print(col, "\t", listings_df[col].dtype)
@@ -128,11 +126,11 @@ listings_df = listings_df.convert_dtypes().assign(
 
 listings_df[["price", "first_review", "last_review"]].dtypes
 
-# %%
+#%%
 # Write clean Datasets to file
 listings_df.to_pickle(path="data-clean/listings.pkl")
 reviews_df.to_pickle(path="data-clean/reviews.pkl")
 calendar_df.to_pickle(path="data-clean/calendar.pkl")
 neighbourhoods_df.to_pickle(path="data-clean/neighbourhoods.pkl")
 
-# %%
+#%%
