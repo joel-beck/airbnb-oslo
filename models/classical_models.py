ce631e7157a620b0e7bc39727b1df45f79dd05d1
Joel Beck <joel.beck@t-online.de>
2022-01-02 15:45:20 +0100

    added more models

--- models/classical_models.py
+++ models/classical_models.py
@@ -18,6 +18,7 @@ from sklearn.ensemble import (
     RandomForestRegressor,
 )
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
@@ -60,17 +61,14 @@ random_state = 123
 
 #%%
 # SECTION: Model Fit & Hyperparameter Search
-model_names = []
-grid_key_list = []
-grid_value_list = []
-
-r2_train_list = []
-r2_test_list = []
-mse_train_list = []
-mse_test_list = []
-
-#%%
 # SUBSECTION: Define Models & Hyperparameters
+# Baseline Model - Mean Price
+# calculated here for all observations, not evaluated on separate test set
+mean_price = y.mean()
+baseline_pred = np.full(shape=y.shape, fill_value=mean_price)
+baseline_r2 = r2_score(y_true=y, y_pred=baseline_pred)
+baseline_mse = mean_squared_error(y_true=y, y_pred=baseline_pred)
+
 linear = LinearRegression()
 linear_pipeline = make_pipeline(preprocessor, linear)
 linear_key = None
@@ -79,12 +77,12 @@ linear_values = None
 lasso = Lasso(random_state=random_state)
 lasso_pipeline = Pipeline([("preprocessor", preprocessor), ("lasso", lasso)])
 lasso_key = "lasso__alpha"
-lasso_values = [1, 5, 10, 20, 50]
+lasso_values = range(10, 60, 10)
 
 ridge = Ridge(random_state=random_state)
 ridge_pipeline = Pipeline([("preprocessor", preprocessor), ("ridge", ridge)])
 ridge_key = "ridge__alpha"
-ridge_values = [1, 5, 10, 20, 50]
+ridge_values = range(10, 60, 10)
 
 # TODO: Generalize Design to multiple hyperparameters in GridSearch
 # TODO: Maybe use RandomizedSearch
@@ -93,17 +91,79 @@ random_forest_pipeline = Pipeline(
     [("preprocessor", preprocessor), ("random_forest", random_forest)]
 )
 random_forest_key = "random_forest__max_depth"
-random_forest_values = [3, 5, 10]
+random_forest_values = range(1, 6)
 
-# TODO: Add additional models
-# ada_boost = AdaBoostRegressor(random_state=random_state)
-# bagging = BaggingRegressor(random_state=random_state)
-# gradient_boosting = GradientBoostingRegressor(random_state=random_state)
+gradient_boosting = GradientBoostingRegressor(random_state=random_state)
+gradient_boosting_pipeline = Pipeline(
+    [("preprocessor", preprocessor), ("gradient_boosting", gradient_boosting)]
+)
+gradient_boosting_key = "gradient_boosting__max_depth"
+gradient_boosting_value = range(1, 6)
 
-models = [linear, lasso, ridge, random_forest]
-pipelines = [linear_pipeline, lasso_pipeline, ridge_pipeline, random_forest_pipeline]
-grid_keys = [linear_key, lasso_key, ridge_key, random_forest_key]
-grid_values = [linear_values, lasso_values, ridge_values, random_forest_values]
+ada_boost = AdaBoostRegressor(random_state=random_state)
+ada_boost_pipeline = Pipeline(
+    [("preprocessor", preprocessor), ("ada_boost", ada_boost)]
+)
+ada_boost_key = "ada_boost__learning_rate"
+ada_boost_value = [0.5, 1, 2, 3, 5]
+
+bagging = BaggingRegressor(random_state=random_state)
+bagging_pipeline = Pipeline([("preprocessor", preprocessor), ("bagging", bagging)])
+bagging_key = "bagging__n_estimators"
+bagging_value = [5, 10, 20, 50]
+
+#%%
+# SUBSECTION: Collect Models
+models = [
+    linear,
+    lasso,
+    ridge,
+    random_forest,
+    gradient_boosting,
+    ada_boost,
+    bagging,
+]
+
+pipelines = [
+    linear_pipeline,
+    lasso_pipeline,
+    ridge_pipeline,
+    random_forest_pipeline,
+    gradient_boosting_pipeline,
+    ada_boost_pipeline,
+    bagging_pipeline,
+]
+
+grid_keys = [
+    linear_key,
+    lasso_key,
+    ridge_key,
+    random_forest_key,
+    gradient_boosting_key,
+    ada_boost_key,
+    bagging_key,
+]
+
+grid_values = [
+    linear_values,
+    lasso_values,
+    ridge_values,
+    random_forest_values,
+    gradient_boosting_value,
+    ada_boost_value,
+    bagging_value,
+]
+
+#%%
+# SUBSECTION: Initialize Results with Baseline Model
+model_names = ["Mean Prediction"]
+grid_key_list = [None]
+grid_value_list = [None]
+
+r2_train_list = [baseline_r2]
+r2_test_list = [baseline_r2]
+mse_train_list = [baseline_mse]
+mse_test_list = [baseline_mse]
 
 #%%
 # SUBSECTION: Fit Models
@@ -122,7 +182,7 @@ for (model, pipeline, grid_key, grid_value) in zip(
         cv = GridSearchCV(
             estimator=pipeline,
             param_grid={grid_key: grid_value},
-            cv=10,
+            cv=5,
             scoring=scoring,
             refit="neg_mean_squared_error",
             return_train_score=True,
@@ -141,7 +201,7 @@ for (model, pipeline, grid_key, grid_value) in zip(
     # if model does not have hyperparameters
     else:
         scores = cross_validate(
-            pipeline, X, y, cv=10, scoring=scoring, return_train_score=True
+            pipeline, X, y, cv=5, scoring=scoring, return_train_score=True
         )
         best_value = None
 
@@ -158,7 +218,7 @@ for (model, pipeline, grid_key, grid_value) in zip(
     mse_train_list.append(mse_train)
     mse_test_list.append(mse_test)
 
-print(f"Finished training in {perf_counter() - start: .2f} seconds")
+print(f"Finished training in {perf_counter() - start:.2f} seconds")
 
 
 #%%
@@ -175,9 +235,11 @@ metrics_df = pd.DataFrame(
     index=model_names,
 )
 
-# NOTE: Performances terrible right now => probably design mistake
+# NOTE: Performances on validation set terrible right now => probably design mistake
 metrics_df.sort_values("r2_test", ascending=False)
 
 #%%
 # save results
 metrics_df.to_pickle("classical_models_results.pkl")
+
+#%%
