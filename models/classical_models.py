f2dbbdbcb2816ee4a16564200e0d3f9d30e81ae1
Joel Beck <joel.beck@t-online.de>
2022-01-02 16:28:41 +0100

    display list of hyperparams correctly in DataFrame

--- models/classical_models.py
+++ models/classical_models.py
@@ -71,46 +71,41 @@ baseline_mse = mean_squared_error(y_true=y, y_pred=baseline_pred)
 
 linear = LinearRegression()
 linear_pipeline = make_pipeline(preprocessor, linear)
-linear_key = None
-linear_values = None
+linear_grid = None
 
 lasso = Lasso(random_state=random_state)
 lasso_pipeline = Pipeline([("preprocessor", preprocessor), ("lasso", lasso)])
-lasso_key = "lasso__alpha"
-lasso_values = range(10, 60, 10)
+lasso_grid = {"lasso__alpha": range(10, 60, 10)}
 
 ridge = Ridge(random_state=random_state)
 ridge_pipeline = Pipeline([("preprocessor", preprocessor), ("ridge", ridge)])
-ridge_key = "ridge__alpha"
-ridge_values = range(10, 60, 10)
+ridge_grid = {"ridge__alpha": range(10, 60, 10)}
 
-# TODO: Generalize Design to multiple hyperparameters in GridSearch
-# TODO: Maybe use RandomizedSearch
+# TODO: Search over multiple hyperparameters and try RandomizedSearch
 random_forest = RandomForestRegressor(random_state=random_state)
 random_forest_pipeline = Pipeline(
     [("preprocessor", preprocessor), ("random_forest", random_forest)]
 )
-random_forest_key = "random_forest__max_depth"
-random_forest_values = range(1, 6)
+random_forest_grid = {
+    "random_forest__n_estimators": [10, 50, 100],
+    "random_forest__max_depth": [1, 3, 5],
+}
 
 gradient_boosting = GradientBoostingRegressor(random_state=random_state)
 gradient_boosting_pipeline = Pipeline(
     [("preprocessor", preprocessor), ("gradient_boosting", gradient_boosting)]
 )
-gradient_boosting_key = "gradient_boosting__max_depth"
-gradient_boosting_value = range(1, 6)
+gradient_boosting_grid = {"gradient_boosting__max_depth": range(1, 6)}
 
 ada_boost = AdaBoostRegressor(random_state=random_state)
 ada_boost_pipeline = Pipeline(
     [("preprocessor", preprocessor), ("ada_boost", ada_boost)]
 )
-ada_boost_key = "ada_boost__learning_rate"
-ada_boost_value = [0.5, 1, 2, 3, 5]
+ada_boost_grid = {"ada_boost__learning_rate": [0.5, 1, 2, 3, 5]}
 
 bagging = BaggingRegressor(random_state=random_state)
 bagging_pipeline = Pipeline([("preprocessor", preprocessor), ("bagging", bagging)])
-bagging_key = "bagging__n_estimators"
-bagging_value = [5, 10, 20, 50]
+bagging_grid = {"bagging__n_estimators": [5, 10, 20, 50]}
 
 #%%
 # SUBSECTION: Collect Models
@@ -134,24 +129,14 @@ pipelines = [
     bagging_pipeline,
 ]
 
-grid_keys = [
-    linear_key,
-    lasso_key,
-    ridge_key,
-    random_forest_key,
-    gradient_boosting_key,
-    ada_boost_key,
-    bagging_key,
-]
-
-grid_values = [
-    linear_values,
-    lasso_values,
-    ridge_values,
-    random_forest_values,
-    gradient_boosting_value,
-    ada_boost_value,
-    bagging_value,
+param_grids = [
+    linear_grid,
+    lasso_grid,
+    ridge_grid,
+    random_forest_grid,
+    gradient_boosting_grid,
+    ada_boost_grid,
+    bagging_grid,
 ]
 
 #%%
@@ -169,19 +154,17 @@ mse_test_list = [baseline_mse]
 # SUBSECTION: Fit Models
 start = perf_counter()
 
-for (model, pipeline, grid_key, grid_value) in zip(
-    models, pipelines, grid_keys, grid_values
-):
+for (model, pipeline, param_grid) in zip(models, pipelines, param_grids):
     print("Fitting", model.__class__.__name__)
     model_names.append(model.__class__.__name__)
 
     scoring = ["r2", "neg_mean_squared_error"]
 
     # if model has hyperparameters
-    if grid_value is not None:
+    if param_grid is not None:
         cv = GridSearchCV(
             estimator=pipeline,
-            param_grid={grid_key: grid_value},
+            param_grid=param_grid,
             cv=5,
             scoring=scoring,
             refit="neg_mean_squared_error",
@@ -189,8 +172,7 @@ for (model, pipeline, grid_key, grid_value) in zip(
         )
         cv.fit(X, y)
 
-        best_value = cv.best_params_[grid_key]
-        best_index = grid_value.index(best_value)
+        best_index = cv.cv_results_["params"].index(cv.best_params_)
 
         r2_train = cv.cv_results_["mean_train_r2"][best_index]
         r2_test = cv.cv_results_["mean_test_r2"][best_index]
@@ -198,25 +180,33 @@ for (model, pipeline, grid_key, grid_value) in zip(
         mse_train = -cv.cv_results_["mean_train_neg_mean_squared_error"][best_index]
         mse_test = -cv.cv_results_["mean_test_neg_mean_squared_error"][best_index]
 
+        hyperparam_key = [key for key in cv.best_params_]
+        hyperparam_value = [value for value in cv.best_params_.values()]
+
+        if len(hyperparam_key) == 1:
+            hyperparam_key = hyperparam_key[0]
+            hyperparam_value = hyperparam_value[0]
+
     # if model does not have hyperparameters
     else:
         scores = cross_validate(
             pipeline, X, y, cv=5, scoring=scoring, return_train_score=True
         )
-        best_value = None
 
         r2_train = np.mean(scores["train_r2"])
         r2_test = np.mean(scores["test_r2"])
         mse_train = -np.mean(scores["train_neg_mean_squared_error"])
         mse_test = -np.mean(scores["test_neg_mean_squared_error"])
 
-    grid_key_list.append(grid_key)
-    grid_value_list.append(best_value)
+        hyperparam_key = None
+        hyperparam_value = None
 
     r2_train_list.append(r2_train)
     r2_test_list.append(r2_test)
     mse_train_list.append(mse_train)
     mse_test_list.append(mse_test)
+    grid_key_list.append(hyperparam_key)
+    grid_value_list.append(hyperparam_value)
 
 print(f"Finished training in {perf_counter() - start:.2f} seconds")
 
