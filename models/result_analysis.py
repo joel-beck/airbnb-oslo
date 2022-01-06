559f3f4f0b404f27cef0e0de4739904b5177cb2f
Joel Beck <joel.beck@t-online.de>
2022-01-06 23:49:49 +0100

    investigated outliers

--- models/result_analysis.py
+++ models/result_analysis.py
@@ -1,8 +1,19 @@
 #%%
+from warnings import simplefilter
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import RFE
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVR
 
+from sklearn_helpers import get_column_transformer, get_preprocessor
+
+simplefilter(action="ignore", category=FutureWarning)
 sns.set_theme(style="whitegrid")
 pd.set_option("precision", 3)
 
@@ -65,5 +76,79 @@ g = sns.relplot(
 
 g.fig.suptitle("R^2")
 g.fig.subplots_adjust(top=0.9)
+
+#%%
+listings_subset = pd.read_pickle("../data-clean/listings_subset.pkl")
+
+X = listings_subset.drop(columns="price")
+y = listings_subset["price"]
+
+#%%
+# BOOKMARK: Price Distribution
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 6))
+sns.histplot(y, ax=ax1).set(title="Original Scale")
+sns.histplot(y, log_scale=True, ax=ax2).set(title="Log Scale")
+plt.show()
+
+#%%
+# One heavy outlier
+print(y.nlargest(10))
+print()
+print(y.nsmallest(10))
+
+#%%
+# SUBSECTION: Fit and Predict with Best Model
+column_transformer = get_column_transformer()
+rfe = RFE(SVR(kernel="linear"), n_features_to_select=30, step=0.5)
+preprocessor = get_preprocessor(column_transformer, rfe)
+best_model = RandomForestRegressor(n_estimators=6, min_samples_leaf=3, max_depth=4)
+
+pipeline = make_pipeline(preprocessor, best_model)
+pipeline.fit(X, y)
+y_hat = pipeline.predict(X)
+
+mean_absolute_error(y, y_hat), r2_score(y, y_hat)
+
+#%%
+# BOOKMARK: Predictions vs. True Price
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 6))
+ax1.plot(
+    [min(y), max(y)],
+    [min(y), max(y)],
+    linestyle="dashed",
+    color="grey",
+)
+pd.DataFrame({"True Price": y.values, "Predictions": y_hat}).plot(
+    kind="scatter", x="True Price", y="Predictions", ax=ax1
+)
+ax1.set(title="Original Scale")
+
+ax2.plot(
+    [min(np.log(y)), max(np.log(y))],
+    [min(np.log(y)), max(np.log(y))],
+    linestyle="dashed",
+    color="grey",
+)
+pd.DataFrame({"True Price": np.log(y.values), "Predictions": np.log(y_hat)}).plot(
+    kind="scatter", x="True Price", y="Predictions", ax=ax2
+)
+ax2.set(title="Log Scale")
+
+plt.show()
+
+#%%
+# SUBSECTION: Fit again with largest price value removed
+largest_price = listings_subset.nlargest(1, "price")
+listings_reduced = listings_subset.drop(largest_price.index)
+
+X = listings_reduced.drop(columns=["price"])
+y = listings_reduced["price"]
+
+pipeline = make_pipeline(preprocessor, best_model)
+pipeline.fit(X, y)
+y_hat = pipeline.predict(X)
+
+mean_absolute_error(y, y_hat), r2_score(y, y_hat)
 
 #%%
