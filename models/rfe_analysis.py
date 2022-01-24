598c6bb50736961418ee9e647a577d6e947a82ca
Joel Beck <joel.beck@t-online.de>
2022-01-24 22:20:45 +0100

    added plot for rfe analysis

--- models/rfe_analysis.py
+++ models/rfe_analysis.py
@@ -27,3 +27,66 @@ complete_rfe_results = pd.concat(
 ).sort_values("mae_val")
 
 complete_rfe_results.to_pickle("../results-pickle/complete_rfe_results.pkl")
+
+#%%
+complete_rfe_results
+
+#%%
+plot_data = complete_rfe_results.astype({"num_features": "category"})
+
+# sharex="col", sharey="row" is amazing :)
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 12), sharex="col", sharey=True)
+ax1, ax2, ax3, ax4 = axes.flat
+
+sns.scatterplot(
+    data=plot_data,
+    x="mae_val",
+    y=plot_data.index,
+    hue="num_features",
+    markers=["s", "o"],
+    s=70,
+    ax=ax1,
+).set(title="MAE Validation", xlabel="")
+
+sns.scatterplot(
+    data=plot_data,
+    x="mae_train",
+    y=plot_data.index,
+    hue="num_features",
+    markers=["s", "o"],
+    s=70,
+    ax=ax3,
+    legend=False,
+).set(title="MAE Training", xlabel="")
+
+sns.scatterplot(
+    data=plot_data,
+    x="r2_val",
+    y=plot_data.index,
+    hue="num_features",
+    markers=["s", "o"],
+    s=70,
+    ax=ax2,
+    legend=False,
+).set(title=r"$R^2$ Validation", xlabel="")
+
+sns.scatterplot(
+    data=plot_data,
+    x="r2_train",
+    y=plot_data.index,
+    hue="num_features",
+    markers=["s", "o"],
+    s=70,
+    ax=ax4,
+    legend=False,
+).set(title=r"$R^2$ Training", xlabel="")
+
+fig.suptitle(
+    "Model Performances for different feature sets\n"
+    "Neural Network fitted with prices on original scale, all other Models fitted with prices on logarithmic scale"
+)
+fig.subplots_adjust(right=0.8, top=0.9)
+
+sns.move_legend(obj=ax1, loc="center", bbox_to_anchor=(2.5, -0.1), frameon=False)
+
+plt.show()
